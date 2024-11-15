cec26351b93e8b5326f75b01b20d678e8806d5a9
Andres Arevalo <andresarevalo@MacBook-Pro-de-Andres.local>
2024-11-14 20:10:44 -0500

    feat: :sparkles: screen to search classrooms without being login

--- js/routes.js
+++ js/routes.js
@@ -1,7 +1,7 @@
 // routes.js
 const routes = {
     "/": "pages/home/index.html",
-    "/about": "pages/aboutUs/index.html",
+    "/classrooms": "pages/searchClassroom/index.html",
     "/contact": "pages/contact/index.html",
     "404": "pages/404.html"
 };
